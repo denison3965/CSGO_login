762b3378f755ce55272cfa0ea5203b9eee268933
Denison <denisondealmeidaprtela@hotmail.com>
2020-10-08 04:43:42 -0300

    terminando a pagina de login

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import Login from './components/login'
+import Video from './components/trailer'
 
 
 function App() {
 
-  useEffect(()=>{
-    axios.get('/teste').then(result => {
-        console.log(result)
-      })
-  },[])
+  // useEffect(()=>{
+  //   axios.get('/teste').then(result => {
+  //       console.log(result)
+  //     })
+  // },[])
 
   return (
     <div className="App">
+      <Video />
       <Login />
+ 
     </div>
   );
 }
